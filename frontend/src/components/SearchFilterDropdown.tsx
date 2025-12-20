55f1c0cea9eedbff445ffc467a725053b0421049
Bassem Kidwai <z5479692@au.unsw.edu.au>
2025-12-20 20:55:56 +1100

    One more case change

--- frontend/src/components/SearchFilterDropdown.tsx
+++ frontend/src/components/SearchFilterDropdown.tsx
@@ -74,7 +74,7 @@ export function SearchFilterDropdown({
           >
             {value
               ? valOptions.find((curr) => curr === value)
-              : `Select ${label} option...`}
+              : `Select ${label} Option...`}
             <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
           </Button>
         </PopoverTrigger>
