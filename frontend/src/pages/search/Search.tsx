3acb3682e03391a3c2b6059e5b93e6e684a21646
Bassem Kidwai <z5479692@au.unsw.edu.au>
2025-12-20 20:56:09 +1100

    Added sidebar to search page

--- frontend/src/pages/search/Search.tsx
+++ frontend/src/pages/search/Search.tsx
@@ -1,10 +1,14 @@
+import Sidebar from "@/components/Sidebar";
 import { RestaurantFilterProvider } from "@/contexts/RestaurantFilterContext";
 import SearchPage from "@/pages/search/SearchPage";
 
 export default function Search() {
   return (
     <RestaurantFilterProvider>
-      <SearchPage />
+      <div className="flex flex-row max-h-screen max-w-screen">
+        <Sidebar />
+        <SearchPage />
+      </div>
     </RestaurantFilterProvider>
   );
 }
