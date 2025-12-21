c0321b4ae1ddb9981b66e0f8766bc5f6cde60eb9
Bassem Kidwai <z5479692@au.unsw.edu.au>
2025-12-21 16:20:20 +1100

    Added a dodgy dark mode toggle, needs to be updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,20 @@ import Dashboard from '@/pages/dashboard/Dashboard';
 import RestaurantPage from '@/pages/restaurantPage/RestaurantPage';
 import Search from '@/pages/search/Search';
 import Profile from '@/pages/profile/Profile';
+import { ThemeProvider } from '@/components/dark-mode/theme-provider';
 
 export default function App() {
 
   return (
-    <div className="app-root">
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/restaurant" element={<RestaurantPage />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </div>
+    <ThemeProvider>
+      <div className="app-root">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/restaurant" element={<RestaurantPage />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
