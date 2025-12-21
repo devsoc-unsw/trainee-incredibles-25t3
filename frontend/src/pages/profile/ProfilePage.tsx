654f5fb2ba8f3aee2b0da7d410cff1703a9a4a22
Bassem Kidwai <z5479692@au.unsw.edu.au>
2025-12-21 12:44:01 +1100

    Updated element sizing, and added basic review section to profile page

--- frontend/src/pages/profile/ProfilePage.tsx
+++ frontend/src/pages/profile/ProfilePage.tsx
@@ -1,10 +1,13 @@
 "use client"
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Item, ItemMedia, ItemTitle } from "@/components/ui/item";
+import { Item, ItemActions, ItemDescription, ItemMedia, ItemTitle } from "@/components/ui/item";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 import { Badge as BadgeSchema, Review, UserProfile } from "@/lib/type";
+
 
 /* -------------------------------- Demo User ------------------------------- */
 
@@ -60,7 +63,7 @@ const user: UserProfile = {
   lastName: "Johnson",
   email: "user@example.com",
   profilePicture:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/960px-Default_pfp.svg.png",
+    "https://tb-static.uber.com/prod/image-proc/processed_images/106015e768d2aa3113a7c8991a9ef9fd/3ac2b39ad528f8c8c5dc77c59abb683d.jpeg",
   xp: 420,
   badges: userBadges,
 };
@@ -72,7 +75,10 @@ export default function ProfilePage() {
       <div className="flex flex-col w-1/2 gap-6">
         <Card className="rounded-3xl shadow-lg flex flex-row">
           <CardHeader className="flex flex-row">
-            <img alt="profile-picture" src={user.profilePicture} className="w-48 h-48" />
+            <Avatar className="size-48 shadow-2xl">
+              <AvatarImage src={user.profilePicture} className="object-cover"/>
+              <AvatarFallback>User Profile</AvatarFallback>
+            </Avatar>
           </CardHeader>
           <CardContent className="flex flex-col pt-8">
             <CardTitle className="flex flex-row pb-4 gap-4">
@@ -104,7 +110,7 @@ export default function ProfilePage() {
               {user.badges.map((badge) => (
                 <Item key={badge._id} variant="outline" className="shadow-sm rounded-3xl mb-2">
                   <ItemMedia>
-                    <div className="text-xl">
+                    <div className="text-3xl">
                       {badge.icon}
                     </div>
                   </ItemMedia>
@@ -119,11 +125,35 @@ export default function ProfilePage() {
         </Card>
       </div>
 
-      {/* Restaurants + Reviews */}
-      <div>
-        <div>
+      <Separator orientation="vertical"/>
 
-        </div>
+      {/* Restaurants + Reviews */}
+      <div className="flex flex-col w-1/2 gap-6 pr-6">
+        <Card className="border-none h-full w-full shadow-none p-0">
+          <CardHeader className="pl-4">
+            <CardTitle>
+              Reviews
+            </CardTitle>
+          </CardHeader>
+          <CardContent className=" pl-4 pr-0">
+            <ScrollArea className="flex flex-col gap-2">
+              {userReviews.map((review) => (
+                <Item key={review._id} variant="outline" className="shadow-sm rounded-3xl mb-2">
+                  <ItemTitle className="text-lg">
+                    {review.restaurant}
+                  </ItemTitle>
+                  <ItemDescription>
+                    {review.content}
+                  </ItemDescription>
+                  <ItemActions>
+
+                  </ItemActions>
+                </Item>
+              ))}
+              <ScrollBar />
+            </ScrollArea>
+          </CardContent>
+        </Card>
         <div>
 
         </div>
